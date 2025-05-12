ebc20081f80a88ce5717da16350a52766c2058fe
Aekard <lorddrakebaza@gmail.com>
2025-05-12 23:07:26 +0800

    pageschange

--- js/nav.js
+++ js/nav.js
@@ -2,3 +2,6 @@ function toggleMenu() {
     const panel = document.getElementById('side-container');
     panel.style.right = panel.style.right === '0px' ? '-250px' : '0px';
 }
+document.getElementById('logo_button').onclick = function() {
+    window.location.href = "../index.html";
+};
